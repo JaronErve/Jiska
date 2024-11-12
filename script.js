1ef8a4cea6be281d7b5cdebe138b8923990e3aec
JaronErve <Jaron.vte@gmail.com>
2024-11-12 13:32:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ function checkNaam() {
             const correctLetter = correctNaam[i];
 
             if (letter === correctLetter) {
-                resultaat += <span style='color: green;'>${letter}</span>; // Correcte letter, groen
+                resultaat += `<span style='color: green;'>${letter}</span>`; // Correcte letter, groen
             } else if (correctNaam.includes(letter)) {
-                resultaat += <span style='color: orange;'>${letter}</span>; // Letter komt voor, maar niet op de juiste plek, oranje
+                resultaat += `<span style='color: orange;'>${letter}</span>`; // Letter komt voor, maar niet op de juiste plek, oranje
             } else {
-                resultaat += <span style='color: red;'>${letter}</span>; // Fout, rood
+                resultaat += `<span style='color: red;'>${letter}</span>`; // Fout, rood
             }
         }
         resultaatElement.innerHTML = resultaat;
