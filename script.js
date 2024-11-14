4a330641d4219eac68eaa0b0cebb9429ac2be27c
JaronErve <Jaron.vte@gmail.com>
2024-11-14 14:14:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,4 +38,36 @@ function checkNaam() {
             } else if (correctNaam.toLowerCase().includes(letter)) {
                 resultaat += `<span style='color: orange;'>${letter}</span>`;
             } else {
-                r
+                resultaat += `<span style='color: red;'>${letter}</span>`;
+            }
+        }
+        resultaatElement.innerHTML = resultaat;
+    }
+}
+
+function showVuurwerk() {
+    const vuurwerkContainer = document.getElementById("vuurwerkContainer");
+    vuurwerkContainer.style.display = "flex";
+    
+    for (let i = 0; i < 30; i++) {
+        const firework = document.createElement("div");
+        firework.classList.add("firework");
+        firework.style.left = Math.random() * 100 + "vw";
+        firework.style.top = Math.random() * 100 + "vh";
+        firework.style.backgroundColor = getRandomColor();
+        vuurwerkContainer.appendChild(firework);
+        
+        firework.addEventListener("animationend", () => {
+            firework.remove();
+        });
+    }
+
+    setTimeout(() => {
+        vuurwerkContainer.style.display = "none";
+    }, 3000);
+}
+
+function getRandomColor() {
+    const colors = ["#ff5733", "#ffbd33", "#dbff33", "#75ff33", "#33ff57", "#33ffbd", "#33dbff", "#3375ff", "#5733ff"];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
